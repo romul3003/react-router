59746abe88e54d97cf89e61aa3fbdaa6d19f5ebe
Roman Zakrevskyi <romul3003@gmail.com>
2019-11-24 20:33:38 +0200

    made routes

---
+++ src/components/Schedule.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Schedule = () => (
+	<div>
+		<ul>
+			<li>6/5 @ Спартак</li>
+			<li>6/8 vs Зенит</li>
+			<li>6/14 @ Рубин</li>
+		</ul>
+	</div>
+)
+
+export default Schedule
